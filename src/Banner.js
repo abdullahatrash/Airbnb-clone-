c669875264b94981aec0ff4806cc16a41d0bd090
abdullahatrash <abodi1987@gmail.com>
2020-08-28 15:28:44 +0200

    second commit

--- src/Banner.js
+++ src/Banner.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./banner.css";
+import { Button } from "@material-ui/core";
+import Search from "./Search";
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="banner">
-      <h1>Banner component </h1>
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          variant="outlined"
+          className="banner__searchButton"
+        >
+          {showSearch ? "Hide" : "Search Date"}
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1>Get out and start your imagination</h1>
+        <h5>
+          Plan a different Kind getaway to uncover the hidden gems near you.{" "}
+        </h5>
+        <Button variant="outlined">Explore Nearby</Button>
+      </div>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./card.css";
+function Card({ src, title, description, price }) {
+  return (
+    <div className="card">
+      <h1>Card</h1>
+    </div>
+  );
+}
+
+export default Card;

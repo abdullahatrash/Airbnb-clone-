355dab74a2ae6187d42e24499e7105440f06319e
abdullahatrash <abodi1987@gmail.com>
2020-08-28 22:32:23 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import Home from "./Home";
 import Header from "./Header.js";
 import Footer from "./Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-
-        <Home />
-
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
         <Footer />
       </Router>
     </div>
